cfdc7e37e6553c37753db61e16614976860f05d3
Your Name <you@example.com>
2026-04-21 17:32:26 +0100

    Esta si k si!

--- app.py
+++ app.py
@@ -46,9 +46,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message = 'Debes iniciar sesión para acceder a esta página.'
 login_manager.init_app(app)
-@app.context_processor
-def inject_user():
-    return dict(current_user=current_user)
+
 # ──────────────────────────────────────────────
 # MODELOS — Usuarios
 # ──────────────────────────────────────────────
